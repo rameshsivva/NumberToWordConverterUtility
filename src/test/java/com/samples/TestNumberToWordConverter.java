37c5c872b39c604bc48f856b9bcb93aa6af1caf2
rameshsivva <rameshsivva@users.noreply.github.com>
2019-11-21 22:16:08 +0530

    Update TestNumberToWordConverter.java
    Added one more unit test

--- src/test/java/com/samples/TestNumberToWordConverter.java
+++ src/test/java/com/samples/TestNumberToWordConverter.java
@@ -7,8 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class TestNumberToWordConverter {
 
 	@Test
-	public void test() {
+	public void testNumber() {
 		assertEquals(NumberToWordConverter.convert(56945781), "fifty six million nine hundred and forty five thousand seven hundred and eighty one");
+	}
+	
+	@Test
+	public void testOutOfRangeNumber() {
 		assertEquals(NumberToWordConverter.convert(-1), "Enter number is out of range [0 and 999,999,999]. Please try again with a number within specified range.");
 	}
 
